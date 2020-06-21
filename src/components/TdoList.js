29dedf2bc608c2ec4f1eee8e94d89bb667d89020
gayoon choi <doll2299@naver.com>
2020-06-21 14:55:00 +0900

    css 수정
    commit

--- src/components/TdoList.js
+++ src/components/TdoList.js
@@ -2,11 +2,16 @@ import React from 'react';
 import TodoItem from './TodoItem'
 // import './Template.css'
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onInsertToogle, onSelectedchangedTodo }) => {
     return(
         <div className="Content_Column_View">
             {todos.map(todo => (
-                <TodoItem todo ={todo} key={todo.id}/>
+                <TodoItem 
+                    todo ={todo} 
+                    key={todo.id} 
+                    onInsertToogle={onInsertToogle}
+                    onSelectedchangedTodo={onSelectedchangedTodo}
+                />
             ))}
         </div>
     )
